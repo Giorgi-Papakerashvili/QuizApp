3b2d7306bb2641b82993e60c3fbc365996dcc673
Giorgi Papakerashvili <giorgi.papakerashvili.1@btu.edu.ge>
2023-04-26 19:22:22 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
+import { questions } from "./Components/Questions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+function App(){
+    const [showFinalResult,setFinalResult]=useState(false)
+    const [score,setScore]=useState(0)
+    const [currentQuestion,setQurrentQuestion]=useState(0)
+
+    const optionClicked=(isCorrect)=>{
+      if(isCorrect){
+        setScore(score+1)             
+      }
+      if(currentQuestion+1<questions.length){
+        setQurrentQuestion(currentQuestion+1)
+      }else{
+        setFinalResult(true)
+      }      
+    }
+  
+
+    const restartQuiz=()=>{
+      setScore(0)
+      setQurrentQuestion(0)
+      setFinalResult(false)
+    }
+    
+    return(
+        <div className="App">
+            <h2 className="currentScore">Current score:{score}</h2>
+            {
+                showFinalResult?
+                <div className="final-results">
+                <h1>Final Results</h1>
+                <h2>{score} out of {questions.length} correct</h2>
+                <h2>{score/questions.length*100}%</h2>
+                <button onClick={()=>restartQuiz()}>Restars Quiz</button>
+            </div>
+            :
+            <div className="question-card">
+                <h2 className="outOf">question {currentQuestion+1} out of {questions.length}</h2>
+                <h3 className="question-text">{questions[currentQuestion].text}</h3>
+                <ul>
+                    {questions[currentQuestion].options.map((option)=>{
+                      return <li onClick={()=>optionClicked(option.isCorrect)} key={option.id}>{option.text}</li>
+                    })}
+                </ul>
+            </div>
+}
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Components/Questions.js
@@ -0,0 +1,92 @@
+export const questions = [
+  {
+    text: "What is the capital of Georgia?",
+    options: [
+      { id: 0, text: "Batumi", isCorrect: false },
+      { id: 1, text: "Mthkheta", isCorrect: false },
+      { id: 2, text: "Kutaisi", isCorrect: false },
+      { id: 3, text: "Tbilisi", isCorrect: true },
+    ],
+  },
+  {
+    text: "What is the capital of England?",
+    options: [
+      { id: 0, text: "London", isCorrect: true },
+      { id: 1, text: "Liverpool", isCorrect: false },
+      { id: 2, text: "Manchester", isCorrect: false },
+      { id: 3, text: "Birmingham", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of France?",
+    options: [
+      { id: 0, text: "Bordeaux", isCorrect: false },
+      { id: 1, text: "Marseille", isCorrect: false },
+      { id: 2, text: "Paris", isCorrect: true },
+      { id: 3, text: "Lyon", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of Canada?",
+    options: [
+      { id: 0, text: "Calgary", isCorrect: false },
+      { id: 1, text: "Ottawa", isCorrect: true },
+      { id: 2, text: "Toronto", isCorrect: false },
+      { id: 3, text: "Montreal", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of China?",
+    options: [
+      { id: 0, text: "Shanghai", isCorrect: false },
+      { id: 1, text: "Guangzhou", isCorrect: false },
+      { id: 2, text: "Beijing", isCorrect: true },
+      { id: 3, text: "Shenzhen", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of Norway?",
+    options: [
+      { id: 0, text: "Oslo", isCorrect: true },
+      { id: 1, text: "Bergen", isCorrect: false },
+      { id: 2, text: "Stavanger", isCorrect: false },
+      { id: 3, text: "Trondheim", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of Italy?",
+    options: [
+      { id: 0, text: "Livorno", isCorrect: false },
+      { id: 1, text: "Verona", isCorrect: false },
+      { id: 2, text: "Milan", isCorrect: false },
+      { id: 3, text: "Rome", isCorrect: true },
+    ],
+  },
+  {
+    text: "What is the capital of USA?",
+    options: [
+      { id: 0, text: "Santa Fe", isCorrect: false },
+      { id: 1, text: "New York City", isCorrect: false },
+      { id: 2, text: "Boston", isCorrect: false },
+      { id: 3, text: "Washington DC", isCorrect: true },
+    ],
+  },
+  {
+    text: "What is the capital of Germany?",
+    options: [
+      { id: 0, text: "Munchen", isCorrect: false },
+      { id: 1, text: "Dortmund", isCorrect: false },
+      { id: 2, text: "Berlin", isCorrect: true },
+      { id: 3, text: "Hamburg", isCorrect: false },
+    ],
+  },
+  {
+    text: "What is the capital of Netherlands?",
+    options: [
+      { id: 0, text: "Groningen", isCorrect: false },
+      { id: 1, text: "Amsterdam", isCorrect: true },
+      { id: 2, text: "Utrecht", isCorrect: false },
+      { id: 3, text: "Rotterdam", isCorrect: false },
+    ],
+  },
+];
